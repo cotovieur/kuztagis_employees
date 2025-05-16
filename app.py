e31bee3a14c0d858d61fc53d54fb5fa2694047de
cotovieur <iliyaklintsov@gmail.com>
2025-05-16 16:29:09 +0700

    Added: confirming of fire

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def remove_worker_from_item():
         return jsonify({'status': 'worker_not_found'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
